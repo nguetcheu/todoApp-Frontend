48f8df04839dfccb1d457273ed318f2fd7662da9
nguetcheu <dnguetcheu@gmail.com>
2024-10-12 22:09:05 +0200

    Affichage de la liste de todos de facon dynamique

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
-  styleUrls: ['./list-todos.component.css']
+  styleUrls: ['./list-todos.component.css'],
 })
-export class ListTodosComponent {
+export class ListTodosComponent implements OnInit {
+  todos = [
+    {
+      id: 1,
+      description: 'Learn to dance',
+    },
+    {
+      id: 2,
+      description: 'Become an Expert in Angular',
+    },
+    {
+      id: 3,
+      description: 'Visit India',
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
