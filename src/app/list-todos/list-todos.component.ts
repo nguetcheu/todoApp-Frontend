120b22dd60131609671b8667e416e274c6d172bd
nguetcheu <dnguetcheu@gmail.com>
2024-10-12 22:29:32 +0200

    Redirection et affichage de la liste de tache en se basant sur la classe todo ok

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../interface/Todo';
 
 @Component({
   selector: 'app-list-todos',
@@ -6,18 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-todos.component.css'],
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
+  todos: Todo[] = [
     {
       id: 1,
-      description: 'Learn to dance',
+      description: 'Learn to Dance',
+      done: false,
+      targetDate: new Date(),
     },
     {
       id: 2,
-      description: 'Become an Expert in Angular',
+      description: 'Become an Expert at Angular',
+      done: false,
+      targetDate: new Date(),
     },
     {
       id: 3,
-      description: 'Visit India',
+      description: 'Visit india',
+      done: false,
+      targetDate: new Date(),
     },
   ];
 
