90e758aa4f96504b28173c435aeef97c35f6ef10
nguetcheu <dnguetcheu@gmail.com>
2024-10-12 23:55:42 +0200

    Gestion de la connexion et de la deconnexion

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -7,9 +7,17 @@ export class HardcodedAuthenticationService {
   constructor() {}
 
   authenticate(username: string, password: string) {
+    // console.log('before' + this.isUserLoggedIn());
     if (username === 'nguetcheu' && password === 'dummy') {
+      sessionStorage.setItem('authenticatedUser', username);
+      // console.log('before' + this.isUserLoggedIn());
       return true;
     }
     return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticaterUser');
+    return !(user === null);
   }
 }
